b43231ba6cd87763babf3aedbc969faf81f6b79c
ceconley <ceconley4@gmail.com>
2019-01-18 01:16:46 -0500

    I messed with the current_user method

--- app/controllers/lineups_controller.rb
+++ app/controllers/lineups_controller.rb
@@ -43,7 +43,7 @@ class LineupsController < OpenReadController
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_contest
+  def set_lineup
     @lineup = current_user.lineups.find(params[:id])
   end
 
